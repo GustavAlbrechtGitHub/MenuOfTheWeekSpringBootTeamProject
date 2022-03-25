421988f2152184464a4a56773b033245e154734c
GustavAlbrechtGitHub <93325097+GustavAlbrechtGitHub@users.noreply.github.com>
2022-03-25 10:01:56 +0100

    Initial commit

---
+++ src/main/java/com/example/menuoftheweekspringbootteamproject/MenuOfTheWeekSpringBootTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.menuoftheweekspringbootteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuOfTheWeekSpringBootTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuOfTheWeekSpringBootTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/menuoftheweekspringbootteamproject/MenuOfTheWeekSpringBootTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menuoftheweekspringbootteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuOfTheWeekSpringBootTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
