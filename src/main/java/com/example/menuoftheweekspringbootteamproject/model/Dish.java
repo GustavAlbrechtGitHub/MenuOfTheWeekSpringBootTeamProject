a5364e2e836f817b5acc9b8679b95c6678a88081
GustavAlbrechtGitHub <93325097+GustavAlbrechtGitHub@users.noreply.github.com>
2022-03-25 11:02:42 +0100

    Added Getters And Setters, fields and constructors To entitys

--- src/main/java/com/example/menuoftheweekspringbootteamproject/model/Dish.java
+++ src/main/java/com/example/menuoftheweekspringbootteamproject/model/Dish.java
@@ -11,12 +11,64 @@ public class Dish {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+    private String name;
+
+    private boolean isVegetarian;
+
+    private int likes;
+
+    private boolean isFavoured;
+
+
     @ManyToMany(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "ingredients_ID")
     ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
 
+    public Dish() {
+    }
+
+    public Dish(String name, boolean isVegetarian, int likes, boolean isFavoured, ArrayList<Ingredient> ingredients) {
+        this.name = name;
+        this.isVegetarian = isVegetarian;
+        this.likes = likes;
+        this.isFavoured = isFavoured;
+        this.ingredients = ingredients;
+    }
+
     public ArrayList<Ingredient> getIngredients() {
         return ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        isVegetarian = vegetarian;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public boolean isFavoured() {
+        return isFavoured;
+    }
+
+    public void setFavoured(boolean favoured) {
+        isFavoured = favoured;
     }
 
     public void setIngredients(ArrayList<Ingredient> ingredients) {
--- src/main/java/com/example/menuoftheweekspringbootteamproject/model/Ingredient.java
+++ src/main/java/com/example/menuoftheweekspringbootteamproject/model/Ingredient.java
@@ -11,10 +11,47 @@ public class Ingredient {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+    private String name;
+
+    private boolean isVegetarian;
 
 
     @ManyToMany(mappedBy = "ingredients", fetch = FetchType.EAGER)
     ArrayList<Dish> dishes = new ArrayList<>();
+
+    public Ingredient() {
+    }
+
+    public Ingredient(Integer id, String name, boolean isVegetarian, ArrayList<Dish> dishes) {
+        this.id = id;
+        this.name = name;
+        this.isVegetarian = isVegetarian;
+        this.dishes = dishes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        isVegetarian = vegetarian;
+    }
+
+    public ArrayList<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(ArrayList<Dish> dishes) {
+        this.dishes = dishes;
+    }
 
     public Integer getId() {
         return id;
